a27798efbcb042cc27e46b02845245e903702a5a
Santiago <esematillajr@gmail.com>
2023-06-08 14:14:28 +0200

    JavaScript terminado, salvo por el detalle de que la moneda está al revés

--- js/index.js
+++ js/index.js
@@ -6,8 +6,41 @@ let flipButton = document.querySelector("#flipButton");
 let resetButton = document.querySelector("#resetButton");
 
 
+
 flipButton.addEventListener("click", () => {
     let i = Math.floor(Math.random() * 2);
     coin.style.animation = "none";
     
+    if(i){
+        setTimeout(function() {
+            coin.style.animation = "spinHeads 3s forwards";
+        }, 100);
+        tails++;
+    }else{
+        setTimeout(function() {
+            coin.style.animation = "spinTails 3s forwards";
+        }, 100);
+        heads++;
+    }
+    setTimeout(updateCount, 3000);
+    disableButton();
 });
+
+function disableButton(){
+    flipButton.disabled = true;
+    setTimeout(function(){
+        flipButton.disabled = false;
+    }, 3000);
+}
+
+function updateCount(){
+    document.querySelector("#headsCount").textContent = `Heads: ${heads}`
+    document.querySelector("#tailsCount").textContent = `Tails: ${tails}`
+}
+
+resetButton.addEventListener('click', () => {
+    coin.style.animation = "none";
+    heads = 0;
+    tails = 0;
+    updateCount();
+})
